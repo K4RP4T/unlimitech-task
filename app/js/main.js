1c582f7ff1be592f6cc02da638d0ede34ccef893
K4RP4T <kactop@st.amu.edu.pl>
2025-08-11 17:14:23 +0200

    refactor: componentize html

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".header").load("html/header.html");
+    $(".navi").load("html/navbar.html");
+    $(".categories").load("html/categories.html");
+    $(".newsletter").load("html/newsletter.html");
+    $(".footer").load("html/footer.html");
+});
--- app/js/promotions.js
+++ app/js/promotions.js
@@ -1,15 +1,20 @@
 $(document).ready(function () {
-    $('.promotions__slider').slick({
-        autoplay: false,
-        autoplaySpeed: 5000,
-        prevArrow: $('.promotions__prev'),
-        nextArrow: $('.promotions__next'),
-        appendDots: $('.promotions__dots'),
-        dots: true,
-        easing: 'ease-in-out',
-        initialSlide: 4,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        speed: 300
+    $(".promotions").load("html/promotions.html", function () {
+        $('.promotions__slider').slick({
+            autoplay: false,
+            autoplaySpeed: 5000,
+            prevArrow: $('.promotions__prev'),
+            nextArrow: $('.promotions__next'),
+            appendDots: $('.promotions__dots'),
+            dots: true,
+            easing: 'ease-in-out',
+            initialSlide: 4,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            speed: 300
+        });
+
+        $(".promotions__item--1").load("html/promotions_item_1.html");
+        $(".promotions__item--2").load("html/promotions_item_2.html");
     });
 });
